f9942095f7d8ae71544fbb4cc0a8528dd46a8787
Pau <masylum@gmail.com>
2011-11-24 20:20:42 +0100

    first commits

---
+++ Readme.md
@@ -0,0 +1,58 @@
+# Backbone.aggregator
+
+A collection that contains pointers to elements to other collections models.
+
+## Use case?
+
+Having a polymorphic collection that just points to existant elements in your state.
+
+For instance having a *Recent activities* that mixes *Tasks* and *Activities*,
+you want to be sure that adding, deleting or changing a Task or Activity will
+update Recent Activities.
+
+## How does it work?
+
+The API is almost the same as `Backbone.Collection`.
+
+  * You must specify which `collection` the model belongs.
+  * Each model must have a `type` attribute.
+  * The aggregator collection must implement the `collections` method. This will return a mapping of `type` and `collection`.
+  * If you want to `get` a model from the aggregator collection you must specify the type too.
+
+``` javascript
+Models.Task = Backbone.Model.extend({initialize: function () {
+  this.collection = tasks;
+}});
+
+Models.Activity = Backbone.Model.extend({initialize: function () {
+  this.collection = activities;
+}});
+
+Collections.Tasks = Backbone.Collection.extend({model: Models.Task});
+Collections.Activities = Backbone.Collection.extend({model: Models.Activity});
+
+Collections.Threads = Backbone.Aggregator.extend({
+  model: function (attr, opt) {
+    return new Models[attr.type](attr, opt);
+  }
+, collections: function () {
+    return {Task: tasks, Activity: activities};
+  }
+});
+
+tasks = new Collections.Tasks();
+activities = new Collections.Activities();
+threads = new Collections.Threads();
+```
+
+## License
+
+(The MIT License)
+
+Copyright (c) 2010-2011 Pau Ramon <masylum@gmail.com>
+
+Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the 'Software'), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED 'AS IS', WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
---
+++ backbone.aggregator.js
@@ -0,0 +1,157 @@
+/**
+ * @class  Backbone.Aggregator
+ * @name   Backbone Aggregator collections
+ * @desc   Implements a collection that aggregates other Backbone Collections
+*/
+(function () {
+
+  var root = this
+    , Aggregator = {}
+    , _ = root._;
+
+  if (!_ && (typeof require !== 'undefined')) {
+    _ = require('underscore')._;
+  }
+
+  /**
+   * Get an object given an id, type or a object containing both
+   *
+   * @param {String|Object} type
+   * @param {Number} id
+   * @return {Object}
+   */
+  Aggregator.get = function (type, id) {
+    if (typeof type === 'object') {
+      id = type.id;
+      type = type.type || type.get('type');
+    }
+
+    if (id === null || !type) {
+      return null;
+    }
+    return this.collections()[type]._byId[id];
+  };
+
+  /**
+   * Resets the collection and proxies the aggregator collection events
+   *
+   * @param {Object} options
+   */
+  Aggregator._reset = function (options) {
+    Backbone.Collection.prototype._reset.call(this);
+    _.each(this.collections(), function (collection) {
+      collection.bind('all', _.bind(this._proxyEvents, this));
+    }, this);
+  };
+
+  /**
+   * Adds a model into the aggregated collection
+   *
+   * @param {Object} model
+   * @param {Object} options
+   * @return {Object} model
+   */
+  Aggregator._add = function (model, options) {
+    options = options || {};
+    model = this._prepareModel(model, options);
+    return model.collection._add(model, options);
+  };
+
+  /**
+   * Adds a model into the aggregator collection
+   *
+   * @param {Object} model
+   * @param {Object} options
+   * @return {Object} model
+   */
+  Aggregator._addToAggregator = function (model, options) {
+    this._byCid[model.cid] = model;
+
+    var index = options.at !== null ? options.at :
+              this.comparator ? this.sortedIndex(model, this.comparator) :
+              this.length;
+
+    this.models.splice(index, 0, model);
+    model.bind('all', this._onModelEvent);
+    this.length++;
+    options.index = index;
+    if (!options.silent) {
+      model.trigger('add', model, this, options);
+    }
+    return model;
+  };
+
+  /**
+   * Remove a model from the aggregated collection
+   *
+   * @param {Object} model
+   * @param {Object} options
+   * @return {Object} model
+   */
+  Aggregator._remove = function (model, options) {
+    options = options || {};
+    model = this.getByCid(model) || this.get(model);
+    return model.collection._remove(model, options);
+  };
+
+  /**
+   * Remove a model from the aggregator collection
+   *
+   * @param {Object} model
+   * @param {Object} options
+   * @return {Object} model
+   */
+  Aggregator._removeFromAggregator = function (model, options) {
+    delete this._byCid[model.cid];
+    var index = this.indexOf(model);
+    this.models.splice(index, 1);
+    this.length--;
+    options.index = index;
+    if (!options.silent) {
+      model.trigger('remove', model, this, options);
+    }
+    model.unbind('all', this._onModelEvent);
+    return model;
+  };
+
+  /**
+   * Prepare a model to be added to a collection
+   *
+   * @param {Object} model
+   * @param {Object} options
+   * @return {Object} model
+   */
+  Aggregator._prepareModel = function (model, options) {
+    if (!(model instanceof Backbone.Model)) {
+      var attrs = model;
+      model = new this.model(attrs);
+      if (model.validate && !model._performValidation(model.attributes, options)) {
+        model = false;
+      }
+    } else if (!model.collection) {
+      throw Error('Aggregator models have to implement a collection');
+    }
+    return model;
+  };
+
+  /**
+   * Proxies an event happening into the aggregated collection to the aggregator
+   *
+   * @param {Object} model
+   * @param {Object} options
+   * @return {Object} model
+   */
+  Aggregator._proxyEvents = function (ev, model, collection, options) {
+    if (ev === 'add' && collection !== this) {
+      this._addToAggregator(model, options);
+    }
+
+    if (ev === 'remove' && collection !== this) {
+      this._removeFromAggregator(model, options);
+    }
+
+    this.trigger.apply(this, arguments);
+  };
+
+  Backbone.Aggregator = Backbone.Collection.extend(Aggregator);
+}).call(this);
